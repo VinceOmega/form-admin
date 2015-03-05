dfc5ef686379a5e1839a1693e0482d18cb18a919
VinceOmega <vince.omega@gmail.com>
2015-03-05 12:46:05 -0500

    Removed sensitive information.

--- config.php
+++ config.php
@@ -5,10 +5,10 @@ $config = array(
 
 
 		'server' => array(
-			'user' => 'u1072494_justin',
-			'pass' => 'd4cg@4646',
-			'data' => 'db1072494_veris_main',
-			'host' => '10.12.3.46'
+			'user' => '',
+			'pass' => '',
+			'data' => '',
+			'host' => ''
 
 	)
 );
